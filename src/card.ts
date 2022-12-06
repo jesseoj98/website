ba5ebc426b69eef4bebc4a73e15cb1d26749c756
jesseojones <jesseoj98@gmail.com>
2022-12-06 09:20:07 -0600

    create card interface

---
+++ src/card.ts
@@ -0,0 +1,19 @@
+/**
+ * Card interface defines information about a card
+ */
+export interface Card {
+    /**
+     * The path to the icon for the card
+     */
+    icon: string;
+    
+    /**
+     * The title of the card
+     */
+    title: string;
+
+    /** 
+     * The description of the card
+     */
+    description: string;
+}
