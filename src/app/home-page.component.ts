9abbb430cbfaf370c33b9df9c2ca6c09940a6176
jesseojones <jesseoj98@gmail.com>
2022-12-06 09:57:25 -0600

    implement cards on HTML

--- src/app/home-page.component.ts
+++ src/app/home-page.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { cards } from 'src/card';
 
 @Component({
   selector: 'home-page-component',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
-  title = 'website';
+export class HomePageComponent implements OnInit {
+  cards: unknown;
+
+  ngOnInit() {
+    this.cards = cards;
+  }
 }
+
