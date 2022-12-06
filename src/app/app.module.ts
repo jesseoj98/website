21afea756eeb0817c19475b9ee74a1bc7184711a
jesseojones <jesseoj98@gmail.com>
2022-12-06 08:37:55 -0600

    rename app component -> home page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomePageComponent]
 })
 export class AppModule { }
