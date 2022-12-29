d16d15135dae0b34fed5364f1bfabf31c3f24679
jesseojones <jesseoj98@gmail.com>
2022-12-29 12:01:08 -0600

    make imports direct & full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { HomePageComponent } from './home-page.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { HomePageComponent } from 'src/app/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { CodingProjectsComponent } from './coding-projects/coding-projects.component';
-import { ContactInfoComponent } from './contact-info/contact-info.component';
-import { BlogComponent } from './blog/blog.component';
-import { ResumeComponent } from './resume/resume.component';
+import { AboutMeComponent } from 'src/app/about-me/about-me.component';
+import { CodingProjectsComponent } from 'src/app/coding-projects/coding-projects.component';
+import { ContactInfoComponent } from 'src/app/contact-info/contact-info.component';
+import { BlogComponent } from 'src/app/blog/blog.component';
+import { ResumeComponent } from 'src/app/resume/resume.component';
 import { RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
