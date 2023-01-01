b9ef45919308b624a422fc4b90b262eaff4905c6
jesseojones <jesseoj98@gmail.com>
2023-01-01 13:45:47 -0600

    add proper source credit to icons

--- src/card.ts
+++ src/card.ts
@@ -18,6 +18,11 @@ export interface Card {
   title: string;
 
   /**
+   * The source of the icon
+   */
+  source?: string;
+
+  /**
    * The description of the card
    */
   description: string;
@@ -28,24 +33,28 @@ export const cards = [
     id: 0,
     icon: 'assets/info.png',
     title: 'About Me',
+    source: 'Info icons created by Stockio - Flaticon https://www.flaticon.com/free-icons/info',
     description: 'Learn more information about me',
   },
   {
     id: 1,
     icon: 'assets/coding.png',
     title: 'Coding Projects',
+    source: 'Coding icons created by Kiranshastry - Flaticon https://www.flaticon.com/free-icons/coding',
     description: 'A look into my coding projects',
   },
   {
     id: 2,
     icon: 'assets/contact.png',
     title: 'Contact Info',
+    source: 'Contact icons created by Cuputo - Flaticon https://www.flaticon.com/free-icons/contact',
     description: 'How to get in touch with me',
   },
   {
     id: 3,
     icon: 'assets/blog.png',
     title: 'Blog',
+    source: 'Blog icons created by BomSymbols - Flaticon https://www.flaticon.com/free-icons/blog',
     description: 'Read my personal thoughts',
   },
 ] as Card[];
