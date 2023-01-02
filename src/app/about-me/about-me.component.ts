570a30380473d53404172d1d3574c4b705afd075
jesseojones <jesseoj98@gmail.com>
2023-01-02 13:59:07 -0600

    add clickable re-routes to text in about-me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CodingProjectsComponent } from '../coding-projects/coding-projects.component';
+import { ContactInfoComponent } from '../contact-info/contact-info.component';
 
 @Component({
   selector: 'app-about-me',
@@ -6,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.css'],
 })
 export class AboutMeComponent implements OnInit {
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  openCodingProjects(): void {
+    this.dialog.closeAll();
+    this.dialog.open(CodingProjectsComponent);
+  }
+
+  openContactInfo(): void {
+    this.dialog.closeAll();
+    this.dialog.open(ContactInfoComponent);
+  }
 }
