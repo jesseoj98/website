7317781bee7e128c6a37d69713d201f1d68dc7aa
jesseojones <jesseoj98@gmail.com>
2023-01-11 19:30:44 -0600

    adjust icons so that onclick it redirects to attrition URL

--- src/card.ts
+++ src/card.ts
@@ -8,19 +8,24 @@ export interface Card {
   id: number;
 
   /**
+   * The source of the icon
+   */
+  source?: string;
+
+  /**
    * The path to the icon for the card
    */
   icon?: string;
 
   /**
+   * What to display for the title/hover of the icon and if icon cannot be loaded
+   */
+  alt?: string;
+
+  /**
    * The title of the card
    */
   title: string;
-
-  /**
-   * The source of the icon
-   */
-  source?: string;
 
   /**
    * The description of the card
@@ -33,28 +38,32 @@ export const cards = [
     id: 0,
     icon: 'assets/info.png',
     title: 'About Me',
-    source: 'Info icons created by Stockio - Flaticon https://www.flaticon.com/free-icons/info',
+    source: 'https://www.flaticon.com/free-icons/info',
+    alt: 'Info icons created by Stockio - Flaticon',
     description: 'Learn more information about me',
   },
   {
     id: 1,
     icon: 'assets/coding.png',
     title: 'Coding Projects',
-    source: 'Coding icons created by Kiranshastry - Flaticon https://www.flaticon.com/free-icons/coding',
+    source: 'https://www.flaticon.com/free-icons/coding',
+    alt: 'Coding icons created by Kiranshastry - Flaticon',
     description: 'A look into my coding projects',
   },
   {
     id: 2,
     icon: 'assets/contact.png',
     title: 'Contact Info',
-    source: 'Contact icons created by Cuputo - Flaticon https://www.flaticon.com/free-icons/contact',
+    source: 'https://www.flaticon.com/free-icons/contact',
+    alt: 'Contact icons created by Cuputo - Flaticon',
     description: 'How to get in touch with me',
   },
   {
     id: 3,
     icon: 'assets/resume.png',
     title: 'Resume',
-    source: 'Portfolio icons created by Freepik - Flaticon https://www.flaticon.com/free-icons/portfolio',
+    source: 'https://www.flaticon.com/free-icons/portfolio',
+    alt: 'Portfolio icons created by Freepik - Flaticon',
     description: 'Click here to view my resume',
-  }
+  },
 ] as Card[];
