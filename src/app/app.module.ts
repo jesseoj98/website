87bec3e16a9c45d5dbc2c4868e891790b37d5e40
jesseojones <jesseoj98@gmail.com>
2023-04-03 18:56:03 -0500

    add ngbootstrap to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HomePageComponent } from 'src/app/home-page.component';
@@ -28,6 +29,7 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatDialogModule,
     MatButtonModule,
+    NgbModule,
     RouterModule.forRoot([]),
   ],
   providers: [],
